d861724f579e2b3153d728246ecbf5e37df4451a
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-30 16:57:48 +0700

    Zero Division Error

--- Practice_Interview_Questions/handlingerror.py
+++ Practice_Interview_Questions/handlingerror.py
@@ -1,5 +1,11 @@
 try:
-    age = int(input("What is your Age?"))
-    print(age)
+    age1 = int(input("What is your Age?"))
+    age2 = int(input("What is your Frined's Age?"))
+    average = age1 / age2
+    print(average)
+
+except ZeroDivisionError:
+    print("Age cannot be Zero")
+   
 except ValueError:
     print("Invalid Value")
